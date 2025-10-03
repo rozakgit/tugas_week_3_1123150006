e1b9a4721722ca8df12ed789e47f8696b11c1bcd
rozakgit <muhammadabdulrozak21@gmail.com>
2025-10-03 19:43:41 +0700

    first commit

---
+++ bin/tugas_week_3_1123150006.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_3_1123150006/tugas_week_3_1123150006.dart' as tugas_week_3_1123150006;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_3_1123150006.calculate()}!');
+}
---
+++ test/tugas_week_3_1123150006_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_3_1123150006/tugas_week_3_1123150006.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
